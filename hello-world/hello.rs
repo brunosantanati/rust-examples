7d20a69ca622a89e3cd8b065945eb355fab2cac3
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-11-13 16:53:58 -0300

    Hello World example.

---
+++ hello-world/hello.rs
@@ -0,0 +1,9 @@
+//https://doc.rust-lang.org/rust-by-example/hello.html
+
+//A binary can be generated using the Rust compiler: rustc.
+//rustc hello.rs
+//rustc will produce a hello binary that can be executed.
+//$ ./hello
+fn main() {
+    println!("Hello World!");
+}
